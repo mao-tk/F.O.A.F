52fa6f4e7d8c66e96e66b0d64fa5a07761558a5e
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-06 06:51:01 +0000

    [Add]mypage ユーザーinfo

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,27 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.save
+    redirect_to mypage_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
