37c47ca9f303add886e5d04bbb79b2bf01ba9176
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-06 04:43:20 +0000

    [Add]ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts,    dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :folders,   dependent: :destroy
-  
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no-image.jpg')
@@ -17,5 +17,11 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit:[width, height]).processed
   end
-  
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
 end
