da796a459d5274f5a740912c80ff2fb5d6f3e554
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-09 02:30:01 +0000

    [Add]ブックマークフォルダのcreate

--- app/controllers/public/folders_controller.rb
+++ app/controllers/public/folders_controller.rb
@@ -1,7 +1,19 @@
 class Public::FoldersController < ApplicationController
+  def create
+    @folder = current_user.folders.new(folder_params)
+    @folder.save!
+    redirect_to user_path(current_user)
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name)
   end
 end
