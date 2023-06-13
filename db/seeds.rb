8a34350a56819f03be14c140c97884e591c25f0a
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-13 07:40:17 +0000

    [Update]seedに初期データ用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,42 @@ Admin.create!(
   email: ENV['ADMIN_EMAIL'],
   password: ENV['ADMIN_PASSWORD']
 )
+
+User.create!(
+  [
+    {
+      email: 'x.ri.mm.s@gmail.com',
+      password: '123456',
+      name: 'mike'
+    }
+  ]
+)
+
+User.create!(
+  [
+    {
+      email: 'x.ri.s@softbank.ne.jp',
+      password: '123456',
+      name: 'まお'
+    }
+  ]
+)
+
+10.times do |n|
+  User.create!(
+    [
+      {
+        email: "customer#{n}@gmail.com",
+        password: '123456',
+        name: "ユーザー#{n}"
+      }
+    ]
+  )
+end
+
+User.all.each do |user|
+  user.posts.create!(
+    title: 'タイトル',
+    body: 'テキストテキストテキストテキスト'
+  )
+end
