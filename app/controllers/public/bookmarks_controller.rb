1c398b94796713099590001ff0d3ec8ef88b20df
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-16 23:40:11 +0000

    [Add]アクセス制限

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class Public::BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     folder = Folder.find(params[:folder_id])
     post = Post.find(params[:post_id])
