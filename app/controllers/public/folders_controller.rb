4a6ddfc82b06e52824f05096676d267d03b66d8d
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-06-21 06:39:28 +0000

    [Update]ブックマークフォルダ編集・削除

--- app/controllers/public/folders_controller.rb
+++ app/controllers/public/folders_controller.rb
@@ -3,19 +3,38 @@ class Public::FoldersController < ApplicationController
 
   def create
     @folder = current_user.folders.new(folder_params)
-    @folder.save!
-    redirect_to user_path(current_user)
+    @folder.save
+    redirect_to request.referer
   end
 
   def index
   end
 
-  def show
-    folder = Folder.find(params[:id])
-    user = User.find(folder.user.id)
+  def update
+    @folder = Folder.find(params[:id])
+    user = User.find(@folder.user.id)
     @posts = user.posts
-    bookmarks = Bookmark.where(user_id: user.id, folder_id: folder.id).pluck(:post_id)
+    bookmarks = Bookmark.where(user_id: user.id, folder_id: @folder.id).pluck(:post_id)
     @bookmark_list = Post.find(bookmarks)
+    if @folder.update(folder_params)
+      redirect_to request.referer
+    else
+      render :show
+    end
+  end
+
+  def show
+    @folder = Folder.find(params[:id])
+    user = User.find(@folder.user.id)
+    @posts = user.posts
+    bookmarks = Bookmark.where(user_id: user.id, folder_id: @folder.id).pluck(:post_id)
+    @bookmark_list = Post.find(bookmarks)
+  end
+
+  def destroy
+    folder = Folder.find(params[:id])
+    folder.destroy
+    redirect_to user_path(current_user)
   end
 
   private
